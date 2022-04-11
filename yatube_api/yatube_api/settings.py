92beb19df1c83008f037a7db5e7cbc2bb66c2096
Павел Калинин <mono-gluk@ya.ru>
2022-04-11 19:43:06 +0300

    С Readme

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -6,7 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
 
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
